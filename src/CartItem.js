40e0184d950ef6bd8cee8e1003490d8c2acb0209
Vishnudewangan <dewanganvish1721@gmail.com>
2022-07-04 22:06:21 +0530

    implemented upto increase functionality using props

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,50 +2,6 @@ import React from 'react';
 
 class CartItem extends React.Component {
 
-    // increaseQuantity () {
-      
-    //     console.log("this",this);
-
-    // }
-    increaseQuantity = ()=>{
-        console.log("this.state", this.state);// arrow function will bind 'this' by default
-
-        //  this.state.qty+=1; /// this will increase on qty but only in object but react don't know that's why it will not rerender, we achieve in two way
-         
-        // // 1. setState form 1 -> here in setState we are passing object , // here we are shallow merging with state
-
-        // this.setState({
-        //      qty: this.state.qty + 1
-        // });
-
-       // 2.setState form 2- here in setState we will be using callback fn, callback will recieve state as prevState and will return property's new value and it will be shallow merging with state  and react will rerencer
-         // if we require then prevState then we use form2 otherwise form1
-         this.setState((prevState) =>{
-            return {
-                qty: prevState.qty + 1
-            } 
-         });
-
-         // if we want to increase item by 3 times  then calling setState 3 times will not help
-    }
-         decreaseQuantity = ()=>{
-             
-                  const { qty } =this.state;// destructuring object
-
-                  if(qty===0)
-                  {
-                    return ;
-                  }
-                  // setState form 2 - using prevState
-
-                  this.setState( (prevState) => {
-                     return {
-                        qty : prevState.qty - 1
-                     }
-                  });
-         }
-        
-
     
     render () {
           
@@ -71,7 +27,7 @@ class CartItem extends React.Component {
                         src="https://cdn-icons-png.flaticon.com/512/992/992651.png" 
                        // onClick={this.increaseQuantity(this)} // if we don't pass 'this' in increaseQuantity function it will give this as 'undefind' and using this will give error , thats why we need to bind the 'this'-> passing this in parameter
                         
-                       onClick={this.increaseQuantity}
+                       onClick={() => this.props.onIncreaseQuantity(this.props.product)}
 
                     />
                     <img
